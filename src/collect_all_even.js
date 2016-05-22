14711d39ee1039b1e74fc8da4ece50bb9517cfa1
lipeishang <1315789454@qq.com>
2016-05-22 14:37:34 +0800

    theeighth

---
+++ src/collect_all_even.js
@@ -0,0 +1,13 @@
+function collect_all_even(collection) {
+    var result = [];
+
+    collection.forEach(function (element) {
+        if(element % 2 === 0){
+            result.push(element);
+        }
+    });
+
+    return result;
+}
+
+module.exports = collect_all_even;
